887799587e3c64cd46472cad6d05050fca83b870
Schlumen <lumen.haendler@web.de>
2023-04-14 16:36:45 +0200

    Implement feature 2 and 3 and tests

--- src/Event.js
+++ src/Event.js
@@ -1,8 +1,30 @@
 import React, { Component } from "react";
 
 class Event extends Component {
-    render() {
-        return <div></div>;
+
+    state = {
+        details: false
     }
+
+    render() {
+        const { event } = this.props;
+
+        return (
+            <div className="event">
+                <h2>{event.summary}</h2>
+                <p>{new Date(event.start.dateTime).toString()}</p>
+                <p>{event.location}</p>
+                {!this.state.details ?
+                    <button onClick={() => this.setState({ details: true })}>show details</button>
+                    : <>
+                        <h3>About Event:</h3>
+                        <a href={event.htmlLink}>See details on Google Calendar</a>
+                        <p>{event.description}</p>
+                        <button onClick={() => this.setState({ details: false })}>hide details</button>
+                    </>
+                }
+            </div>
+        )
+    };
 }
 export default Event;
