e11c341cc9cbafd0b11331ba4d575f634e00cd17
Schlumen <lumen.haendler@web.de>
2023-04-15 09:24:06 +0200

    Fix locations prop

--- src/App.js
+++ src/App.js
@@ -4,11 +4,12 @@ import EventList from "./EventList";
 import CitySearch from "./CitySearch";
 import NumberOfEvents from "./NumberOfEvents";
 import { mockData } from "./mock-data";
+import { extractLocations } from "./api";
 
 function App() {
     return (
         <div className="App">
-            <CitySearch />
+            <CitySearch locations={extractLocations(mockData)} />
             <NumberOfEvents />
             <EventList events={mockData} />
         </div>
