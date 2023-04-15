0d923e0a2e596da088fcdaa66e25716c8e52e2fc
Schlumen <lumen.haendler@web.de>
2023-04-15 12:26:04 +0200

    Add more features

--- src/App.js
+++ src/App.js
@@ -1,19 +1,49 @@
-import React from "react";
+import React, { Component } from "react";
 import "./App.css";
 import EventList from "./EventList";
 import CitySearch from "./CitySearch";
 import NumberOfEvents from "./NumberOfEvents";
-import { mockData } from "./mock-data";
-import { extractLocations } from "./api";
+import { getEvents, extractLocations } from "./api";
 
-function App() {
-    return (
-        <div className="App">
-            <CitySearch locations={extractLocations(mockData)} />
-            <NumberOfEvents />
-            <EventList events={mockData} />
-        </div>
-    );
+class App extends Component {
+    state = {
+        events: [],
+        locations: []
+    }
+
+    componentDidMount() {
+        this.mounted = true;
+        getEvents().then((events) => {
+            if (this.mounted) {
+                this.setState({ events, locations: extractLocations(events) });
+            }
+        });
+    }
+
+    componentWillUnmount() {
+        this.mounted = false;
+    }
+
+    updateEvents = (location) => {
+        getEvents().then((events) => {
+            const locationEvents = (location === "all") ?
+                events :
+                events.filter((event) => event.location === location);
+            this.setState({
+                events: locationEvents
+            });
+        });
+    }
+
+    render() {
+        return (
+            <div className="App" >
+                <CitySearch locations={this.state.locations} updateEvents={this.updateEvents} />
+                <NumberOfEvents />
+                <EventList events={this.state.events} />
+            </div>
+        );
+    }
 }
 
 export default App;
