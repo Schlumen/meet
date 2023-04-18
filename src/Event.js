b080a183d4736048738b11915bfc52c2254cc01c
Schlumen <lumen.haendler@web.de>
2023-04-18 15:34:13 +0200

    Implement Acceptance and End-to-End Tests

--- src/Event.js
+++ src/Event.js
@@ -16,12 +16,12 @@ class Event extends Component {
                 <p>{event.location}</p>
                 {!this.state.details ?
                     <button className="details-btn" onClick={() => this.setState({ details: true })}>show details</button>
-                    : <>
+                    : <div className="event__Details">
                         <h3>About Event:</h3>
                         <a href={event.htmlLink}>See details on Google Calendar</a>
                         <p>{event.description}</p>
                         <button className="details-btn" onClick={() => this.setState({ details: false })}>hide details</button>
-                    </>
+                    </div>
                 }
             </div>
         )
