43c3103c0a7867f66e2ce3a250d87a8bfbc932a5
Schlumen <lumen.haendler@web.de>
2023-04-19 18:43:54 +0200

    Add Atatus, modify title, remove icon

--- src/index.js
+++ src/index.js
@@ -4,6 +4,8 @@ import './index.css';
 import App from './App';
 import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 import reportWebVitals from './reportWebVitals';
+import * as atatus from 'atatus-spa';
+atatus.config('d857b328e9b747618fad9cf9310acd37').install();
 
 render(
     <React.StrictMode>
@@ -20,3 +22,5 @@ serviceWorkerRegistration.unregister();
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
+
+// atatus.notify(new Error('Test Atatus Setup'));
